80a983564f0745d8417bfe5365861fd622ba6e22
Developer1702 <usmonov1702@gmail.com>
2024-09-27 16:04:11 +0500

    Data added

--- src/components/Collapse.vue
+++ src/components/Collapse.vue
@@ -1,71 +1,38 @@
 <template>
   <div class="w-full h-[100vh] flex flex-col items-center">
     <div class="flex flex-col w-[30%] bg-gray-200 mt-10">
-      <!-- Models bo'limi -->
-      <div>
-        <div
-          class="flex items-center px-2 py-1 gap-x-4 w-full cursor-pointer border-l-4 border-transparent hover:border-blue-500 hover:bg-gray-300"
-          @click="openFunc('models')">
-          <img src="../assets/images/dropdown.svg" alt="" class="w-3 transition"
-            :class="isModel ? 'rotate-0' : 'rotate-[-90deg]'">
-          <span class="text-[24px]">Models</span>
-        </div>
-
-        <!-- Data Warehouse bo'limi -->
-        <div v-if="isModel" @click="openFunc('data')">
-          <div
-            class="flex items-center px-2 py-1 gap-x-4 w-full cursor-pointer pl-[10%] border-l-4 border-transparent hover:border-blue-500 hover:bg-gray-300">
-            <img src="../assets/images/dropdown.svg" alt="" class="w-3 transition"
-              :class="isData ? 'rotate-0' : 'rotate-[-90deg]'">
-            <span class="text-[24px]">Data Warehouse</span>
-          </div>
-          <div v-if="isData" class="flex flex-col">
-            <span
-              class="text-[24px] pl-[30%] w-full cursor-pointer border-l-4 border-transparent hover:border-blue-500 hover:bg-gray-300">Report
-              Samples</span>
-            <span
-              class="text-[24px]  pl-[30%] w-full cursor-pointer border-l-4 border-transparent hover:border-blue-500 hover:bg-gray-300">Sales
-              Performance</span>
-          </div>
-        </div>
-      </div>
-
-      <!-- Statistics bo'limi -->
-      <div>
-        <div
-          class="flex items-center px-2 py-1 gap-x-4 w-full cursor-pointer border-l-4 border-transparent hover:border-blue-500 hover:bg-gray-300"
-          @click="openFunc('stats')">
-          <img src="../assets/images/dropdown.svg" alt="" class="w-3 transition"
-            :class="isStats ? 'rotate-0' : 'rotate-[-90deg]'">
-          <span class="text-[24px]">Statistics</span>
-        </div>
-        <div v-if="isStats" class="flex flex-col">
-          <span
-            class="text-[24px]  pl-[30%] w-full cursor-pointer border-l-4 border-transparent hover:border-blue-500 hover:bg-gray-300">Company
-            statistics</span>
-          <span
-            class="text-[24px]  pl-[30%] w-full cursor-pointer border-l-4 border-transparent hover:border-blue-500 hover:bg-gray-300">Other
-            statistics</span>
-        </div>
-      </div>
+      <TreeCollapse :data="menuData" />
     </div>
   </div>
 </template>
 
-
 <script setup>
 import { ref } from 'vue';
-const isModel = ref(false);
-const isData = ref(false);
-const isStats = ref(false);
-
-const openFunc = (section) => {
-  if (section === 'models') {
-    isModel.value = !isModel.value;
-  } else if (section === 'data') {
-    isData.value = !isData.value
-  } else if (section === 'stats') {
-    isStats.value = !isStats.value
+import TreeCollapse from './TreeCollapse.vue';
+const menuData = ref([
+  {
+    title: 'Models',
+    child: [
+      {
+        title: 'Data Warehouse',
+        child: [
+          { title: 'Report Samples' },
+          { title: 'Sales Performance' },
+        ]
+      }
+    ]
+  },
+  {
+    title: 'Statistics',
+    child: [
+      { title: 'Company statistics' },
+      {
+        title: 'Other statistics',
+        child: [
+          { title: 'Crocodile' }
+        ]
+      },
+    ]
   }
-}
+]);
 </script>
---
+++ src/components/TreeCollapse.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div v-for="(data, i) in data" :key="i" class="flex flex-col">
+      <div
+        class="flex items-center px-2 py-1 gap-x-4 w-full cursor-pointer border-l-4 border-transparent hover:border-blue-500 hover:bg-gray-300"
+        @click="openCollapse(i)">
+        <img v-if="data.child" src="../assets/images/dropdown.svg" alt="" class="w-3 transition"
+          :class="opened[i] ? 'rotate-0' : 'rotate-[-90deg]'">
+        <span class="text-[24px]">{{ data.title }}</span>
+      </div>
+
+      <div v-if="data.child && opened[i]" class="pl-[10%]">
+        <TreeCollapse :data="data.child" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const props = defineProps({
+  data: Array
+});
+
+const opened = ref([]);
+
+onMounted(() => {
+  props.data.forEach(() => {
+    opened.value.push(false);
+  });
+});
+
+const openCollapse = (i) => {
+  opened.value[i] = !opened.value[i];
+};
+</script>
